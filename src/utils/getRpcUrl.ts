e10ff2490cae382c5daa39518d12df34d4bbe9ce
Smart World <smartworldapplication@gmail.com>
2021-10-09 07:22:15 +0330

    fix

--- src/utils/getRpcUrl.ts
+++ src/utils/getRpcUrl.ts
@@ -1,7 +1,11 @@
 import sample from 'lodash/sample'
 
 // Array of available nodes to connect to
-export const nodes = [process.env.REACT_APP_NODE_1, process.env.REACT_APP_NODE_2, process.env.REACT_APP_NODE_3]
+export const nodes = [
+  'https://bsc-dataseed1.ninicoin.io',
+  'https://bsc-dataseed1.ninicoin.io',
+  'https://bsc-dataseed1.ninicoin.io',
+]
 export const testNodes = [
   process.env.REACT_APP_TEST_NODE_1,
   process.env.REACT_APP_TEST_NODE_2,
